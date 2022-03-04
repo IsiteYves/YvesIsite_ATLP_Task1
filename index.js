4a7fb521e602ebf2cf54ab534a29472306fe4130
IsiteYves <yvesisite@gmail.com>
2022-03-04 21:59:05 +0200

    Completed the whole CV Template design

---
+++ index.js
@@ -0,0 +1,17 @@
+const personName = document.querySelector("#person-name");
+
+const generateRandomColor = () => {
+  const maxValue = 0xffffff;
+  let randomNumber = Math.random() * maxValue;
+  randomNumber = Math.floor(randomNumber);
+  randomNumber = randomNumber.toString(16);
+  const randomColor = randomNumber.padStart(6, 0);
+  return randomColor;
+};
+
+const changeHeaderColor = () => {
+  document.documentElement.style.setProperty(
+    "--headerColor",
+    `#${generateRandomColor()}`
+  );
+};
